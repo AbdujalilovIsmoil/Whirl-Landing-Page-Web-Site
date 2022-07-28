4b79860bb6187f58f0208b74e9f642bbce2105ba
= <ismoilbek20050421@gmail.com>
2022-07-28 12:59:35 +0500

    Whirl

--- JS/index.js
+++ JS/index.js
@@ -5,4 +5,11 @@ window.addEventListener("load",()=>{
     let header = document.querySelector(".header");
     header.classList.toggle("active",window.scrollY > 0);
   });
+  let accordionIcons = document.querySelectorAll(".section__accordion_small_box_content_strelka");
+  let accordionParagraph = document.querySelectorAll(".section__accordion_small_box_paragraph");
+  for(let i = 0; i < accordionIcons.length; i++){
+    accordionIcons[i].addEventListener("click",()=>{
+    accordionParagraph[i].classList.toggle("active");
+    });
+  }
 });
